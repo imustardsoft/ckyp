0ddc5cd944d01d4d8d17afcc32e45978dd22f9cb
Jack Xu <jackxxu@gmail.com>
2012-06-25 18:51:19 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ckyp::Application.config.secret_token = 'a99e8a512821465aafeef90d1a54c3865a3e36c5d0aa206157bde4494d1f894a4b73338bda5efff772f1cf26ce6c2180a9243560e8220009899f7a96060ce3ec'
